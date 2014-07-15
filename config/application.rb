8b42c167b20626537580d15a6cad2625493fe87e
Steve Loveless <steve.loveless@gmail.com>
2014-07-15 12:42:04 -0700

    Converted double quotes to single

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../boot', __FILE__)
-require "rails"
+require 'rails'
 
 %w(
   neo4j
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
 
@@ -12,7 +12,7 @@ require 'shoulda/matchers'
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   # RSpec Rails can automatically mix in different behaviours to your tests
