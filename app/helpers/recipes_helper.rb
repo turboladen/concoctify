aa50407910ec94f51a8efd30e3a580b98e647e2c
Steve Loveless <sloveless@sloveless-mbp.local>
2015-10-21 22:00:51 -0700

    Show recipe ingredients

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+  # @param ingredient [Ingredient]
+  # @return [String]
+  def ingredient_line_item(ingredient)
+    rel = ingredient.rels.first
+
+    "#{rel.quantity} #{rel.quantity_unit} #{ingredient.name}".strip
+  end
 end
