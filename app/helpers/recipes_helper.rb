858eeb716a9ce313b4e728ea9930e44569d15c2b
Steve Loveless <sloveless@sloveless-mbp.local>
2015-10-26 21:44:17 -0700

    Use collection_select

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -6,4 +6,10 @@ module RecipesHelper
 
     "#{rel.quantity} #{rel.quantity_unit} #{ingredient.name}".strip
   end
+
+  # @param recipe_id [String]
+  # @return [Array<Array<String, String>>]
+  def other_recipes_for_select(recipe_id)
+    Recipe.where_not(id: recipe_id)
+  end
 end
