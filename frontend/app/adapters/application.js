a0c027cc0ad2f5500f9dcfbcd81ee0a5485163c5
Steve Loveless <sloveless@sloveless-mbp.local>
2015-10-29 09:55:11 -0700

    Allow csrf stuff

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,6 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
+  headers: {
+    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
+  },
   host: 'http://concoctify.dev',
   pathForType: function(type) {
     return `${Ember.String.underscore(type)}s`;
